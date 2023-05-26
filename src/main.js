0c7dc15935cfe2b3ccc2551aae5bd7541e0bf1fc
wuxiaolin <xiaolin.wu@eeoa.com>
2023-05-26 09:28:41 +0800

    feat: 添加相关模块
    Signed-off-by: wuxiaolin <xiaolin.wu@eeoa.com>

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import dataV from "@jiaminghi/data-view";
+import "./main.css";
 
 Vue.config.productionTip = false;
 Vue.use(dataV);
